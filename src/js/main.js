15d3d0730b8fdb087c8ee7b66ed48c24e40016de
senyqy <senyqy@gmail.com>
2023-05-18 23:43:11 +0200

    added responsive to products-list, added pagination and footer

--- src/js/main.js
+++ src/js/main.js
@@ -53,23 +53,52 @@ $(".add-more").on("click", function () {
 });
 
 let slider = document.getElementById('slider');
+let nouisliderInstance;
 
-noUiSlider.create(slider, {
-    start: [4],
-    range: {
-    'min': 1,
-    'max': 4
-    },
-    step: 1,
-    // direction: 'rtl',
-    // connect: true,
-    connect:'upper',
-});
+function createSlider() {
+    nouisliderInstance = noUiSlider.create(slider, {
+        start: [4],
+        connect: true,
+        range: {
+            'min': 4,
+            'max': 6
+        },
+        step: 1,
+        direction: 'rtl',
+        connect:'upper',
+    });
+}
+
+function updateSliderRange(min, max) {
+    nouisliderInstance.updateOptions({
+        range: {
+        'min': min,
+        'max': max
+        }
+    });
+}
+
+createSlider();
+
+var mediaQuery = window.matchMedia('(max-width: 768px)');
+
+function handleMediaQueryChange(mediaQuery) {
+    if (mediaQuery.matches) {
+        // Change min and max values for smaller viewport
+        updateSliderRange(1, 3);
+    } else {
+        // Restore original min and max values
+        updateSliderRange(4, 6);
+    }
+}
+
+mediaQuery.addListener(handleMediaQueryChange);
+handleMediaQueryChange(mediaQuery); // Call the handler initially
 
 slider.noUiSlider.on('update', function(values, handle) {
     var numColumns = parseInt(values[handle]);
-    var columnWidth = 100 / numColumns + '%';
-  
+    var columnWidth = 100 / numColumns - 1 + '%';
+    
     var productGrid = document.querySelector('.products-list');
     productGrid.style.gridTemplateColumns = 'repeat(' + numColumns + ', ' + columnWidth + ')';
 });
