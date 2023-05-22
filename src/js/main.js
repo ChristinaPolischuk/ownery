b1c8297aafdc68e876b95fd7e0e81c95bb3d10d9
senyqy <senyqy@gmail.com>
2023-05-22 15:20:14 +0200

    added menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,25 @@
-// Мобильное меню бургер
-function burgerMenu() {
-    const burger = document.querySelector('.burger')
-    const menu = document.querySelector('.menu')
-    const body = document.querySelector('body')
-    burger.addEventListener('click', () => {
-        if (!menu.classList.contains('active')) {
-            menu.classList.add('active')
-            burger.classList.add('active')
-            body.classList.add('locked')
-        } else {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
-    // Вот тут мы ставим брейкпоинт навбара
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 991.98) {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
-}
-burgerMenu();
+$(document).on("click", ".burger, .burger-close", function() {
+    if($(this).hasClass("burger")) {
+        $(this).closest(".menu").addClass("active");
+        $("body").addClass("locked");
+        $("body").css("padding-right", `${getScrollbarWidth()}px`);
+    } else if ($(this).hasClass("burger-close")) {
+        $(this).closest(".menu").removeClass("active");
+        $("body").removeClass("locked");
+        $("body").css("padding-right", "");
+    }
+});
 
 (function($){
-    $(window).on("load",function(){
+    $(window).on("load", function() {
         $(".dropdown-menu").mCustomScrollbar({
+            axis:"y",
+            theme:"dark-3",
+            scrollButtons:{
+                enable:true
+            }
+        });
+        $(".menu-nav__body").mCustomScrollbar({
             axis:"y",
             theme:"dark-3",
             scrollButtons:{
@@ -102,3 +94,69 @@ slider.noUiSlider.on('update', function(values, handle) {
     var productGrid = document.querySelector('.products-list');
     productGrid.style.gridTemplateColumns = 'repeat(' + numColumns + ', ' + columnWidth + ')';
 });
+
+const CLASS_LIST = {
+    MODAL: "modal",
+    MODAL_ACTIVE: "modal--active",
+    MODAL_DIALOG: "modal__dialog",
+    TRIGGER_OPEN: "js-modal-open",
+    TRIGGER_CLOSE: "js-modal-close"
+};
+
+const showScroll = (event) => {
+    if (event.propertyName === "opacity") {
+        document.body.style.paddingRight = "";
+        document.body.style.overflow = "visible";
+        event.target
+            .closest(`.${CLASS_LIST.MODAL}`)
+            .removeEventListener("transitionend", showScroll);
+    }
+};
+
+document.addEventListener("click", (event) => {
+    //open
+    if (event.target.closest(`.${CLASS_LIST.TRIGGER_OPEN}`)) {
+        event.preventDefault();
+
+        const target = event.target.closest(`.${CLASS_LIST.TRIGGER_OPEN}`);
+        const modalId = target.getAttribute("data-modal");
+        const modal = document.getElementById(modalId);
+
+        document.body.style.paddingRight = `${getScrollbarWidth()}px`;
+        document.body.style.overflow = "hidden";
+
+        modal.classList.add(CLASS_LIST.MODAL_ACTIVE);
+    }
+
+    //close
+    if (
+        event.target.closest(`.${CLASS_LIST.TRIGGER_CLOSE}`) ||
+        (event.target.closest(`.${CLASS_LIST.MODAL}`) != null &&
+            event.target
+                .closest(`.${CLASS_LIST.MODAL}`)
+                .classList.contains(CLASS_LIST.MODAL_ACTIVE) &&
+            !event.target.closest(`.${CLASS_LIST.MODAL_DIALOG}`))
+    ) {
+        event.preventDefault();
+        const modal = event.target.closest(`.${CLASS_LIST.MODAL}`);
+        modal.classList.remove(CLASS_LIST.MODAL_ACTIVE);
+        modal.addEventListener("transitionend", showScroll);
+    }
+});
+
+const getScrollbarWidth = () => {
+    const item = document.createElement("div");
+
+    item.style.position = "absolute";
+    item.style.top = "-9999px";
+    item.style.width = "50px";
+    item.style.height = "50px";
+    item.style.overflow = "scroll";
+    item.style.visibility = "hidden";
+
+    document.body.appendChild(item);
+    const scrollbarWidth = item.offsetWidth - item.clientWidth;
+    document.body.removeChild(item);
+
+    return scrollbarWidth;
+};
