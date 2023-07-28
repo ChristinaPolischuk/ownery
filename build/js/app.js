20aa1b88a1900c9c0f6e4e43a0aecf28a625bb1c
senyqy <senyqy@gmail.com>
2023-07-28 14:53:16 +0200

    language

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,8 @@
+$(window).on("load", function() {
+  $(document).on( "click", ".language-btn", function() {
+    $(this).parent().toggleClass("is-opened");
+    return false;
+  });
 
+  $(".language-dropdown").mCustomScrollbar();
+});
