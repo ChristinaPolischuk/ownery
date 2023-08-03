0548c1cb37f5bfd390935777f404fb1194b90b19
senyqy <senyqy@gmail.com>
2023-08-03 14:23:18 +0200

    Updates

--- js/app.js
+++ js/app.js
@@ -5,4 +5,48 @@ $(window).on("load", function() {
   });
 
   $(".language-dropdown").mCustomScrollbar();
+  $(".mobile-menu__dropdown-inner").mCustomScrollbar();
+
+  $(document).on( "click", ".hamburger", function() {
+    if($(".mobile-menu__navitem.dropdown").hasClass("sub-open")) {
+      $(".mobile-menu__navitem.dropdown").removeClass("sub-open");
+    }
+    if($(this).closest(".mobile-menu").hasClass("is-opened")) {
+      $(this).closest(".mobile-menu").removeClass("is-opened");
+      $("body").removeClass("locked");
+      $("body").css("padding-right", "");
+    } else {
+      $(this).closest(".mobile-menu").addClass("is-opened");
+      $("body").addClass("locked");
+      $("body").css("padding-right", `${getScrollbarWidth()}px`);
+    }
+    
+    return false;
+  });
+
+  $(document).on('click', '.mobile-menu__navtrigger, .mobile-menu__subtrigger', function() {
+    $(this).parents('li').addClass('sub-open');
+    return false;
+  });
+  $(document).on('click', '.mobile-menu__backtrigger', function(){
+    $(this).closest('li').removeClass('sub-open');
+    return false;
+  });
 });
+
+function getScrollbarWidth () {
+  const item = document.createElement("div");
+
+  item.style.position = "absolute";
+  item.style.top = "-9999px";
+  item.style.width = "50px";
+  item.style.height = "50px";
+  item.style.overflow = "scroll";
+  item.style.visibility = "hidden";
+
+  document.body.appendChild(item);
+  const scrollbarWidth = item.offsetWidth - item.clientWidth;
+  document.body.removeChild(item);
+
+  return scrollbarWidth;
+};
